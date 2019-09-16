9bdfff91dcb5f820f6b8aecf91c53c2555bdf44c
Maciej <mactob25@gmail.com>
2019-09-16 20:25:42 +0200

    Initial commit

---
+++ card-game-frontend/build/precache-manifest.8cf05dad1f9d731962489d0896f9c310.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8e90d8ecb20c44b23a6a1296fcd5275",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ffaa09249d75c599b1e5",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "3d6541465990eb970440",
+    "url": "/static/js/2.b89d6928.chunk.js"
+  },
+  {
+    "revision": "ffaa09249d75c599b1e5",
+    "url": "/static/js/main.771ae511.chunk.js"
+  },
+  {
+    "revision": "1a8eee9e89c4a8df16ee",
+    "url": "/static/js/runtime~main.f4eede3b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ card-game-frontend/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return (
+        <div className="card-game-table">
+            <div className="top split-vertically">
+                <p>
+                    React app
+                </p>
+            </div>
+            <div className="bottom split-vertically">
+
+            </div>
+        </div>
+    );
+}
+
+export default App;
